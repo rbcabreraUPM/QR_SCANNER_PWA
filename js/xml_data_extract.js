26785198e9cea8ff240923abbbcdb4967e82be0b
rbcabreraUPM <rbcabrera1@up.edu.ph>
2018-08-30 09:10:54 +0800

    Update xml_data_extract.js

--- js/xml_data_extract.js
+++ js/xml_data_extract.js
@@ -92,7 +92,7 @@ function get_data(xml) {
         $('.image-editor').cropit({
           imageState: {
             src: 'img/user.png',
-            crossorigin: "anonymous"
+      
           },
         });
     }
